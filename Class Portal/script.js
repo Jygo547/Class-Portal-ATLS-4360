aabbf1cddcf1763946b7ef745a5bad3b3a7ee812
Jyotish547 <110929286+Jygo547@users.noreply.github.com>
2023-09-18 15:00:38 -0600

    Nav Menu Responsiveness completed

--- Class Portal/script.js
+++ Class Portal/script.js
@@ -1,6 +1,7 @@
 const menuButton = document.getElementById("mobile-menu");
 const navbar = document.getElementById("navbar-links");
 const close = document.getElementById("close-nav");
+const menu = document.getElementById("menu")
 
 console.log(navbar);
 
@@ -8,4 +9,12 @@ menuButton.addEventListener('click', () => {
     navbar.style.display = navbar.style.display === 'flex' ? 'none' : 'flex';
     menuButton.style.display = menuButton.style.display === 'none' ? 'block' : 'none';
     close.style.display = close.style.display === 'flex' ? 'none' : 'flex';
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 });
+
+close.addEventListener('click', () => {
+    navbar.style.display = navbar.style.display === 'none' ? 'flex' : 'none';
+    menuButton.style.display = menuButton.style.display === 'flex' ? 'none' : 'flex';
+    menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+    close.style.display = close.style.display === 'none' ? 'flex' : 'none';
+})
