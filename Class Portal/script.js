befd2a19aaa68cba351ee3ec7fbcbbc7226b3d26
Jyotish547 <110929286+Jygo547@users.noreply.github.com>
2023-09-19 02:57:44 -0600

    Final Changes before First Host
    Class Portal ready for Milestone 2

--- Class Portal/script.js
+++ Class Portal/script.js
@@ -1,7 +1,9 @@
 const menuButton = document.getElementById("mobile-menu");
 const navbar = document.getElementById("navbar-links");
 const close = document.getElementById("close-nav");
-const menu = document.getElementById("menu")
+const menu = document.getElementById("menu");
+
+
 
 console.log(navbar);
 
@@ -17,6 +19,20 @@ close.addEventListener('click', () => {
     menuButton.style.display = menuButton.style.display === 'flex' ? 'none' : 'flex';
     menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
     close.style.display = close.style.display === 'none' ? 'flex' : 'none';
-})
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const errorAlert = document.getElementById("profile");
+
+//This did not run
+
+    errorAlert.addEventListener('click', () => {
+        alert('This page is not added yet.');
+    
+        window.location.href = 'index.html';
+    })
+    
+});
 
 // Error Note: The navbar toggle button works fine when the screen size is reduced, but after interacting with the navbar and increasing the size, the navbar doesn't show normally and the toggle button remains.
